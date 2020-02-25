d8e666db6e57c96e8bd04a3b205f877f25a3e60a
sabdi2517 <58014586+sabdi2517@users.noreply.github.com>
2020-02-25 11:49:40 -0500

    Completed Unit Tests

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA_A02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool meneBool = false;
+            string userSelection;
+            int Side1;
+            int Side2;
+            int Side3;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("1. Enter Triangle dimensions");
+                    Console.WriteLine("2. Exit");
+
+                    userSelection = Convert.ToString(Console.ReadLine());
+
+                    if (userSelection == "1")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Please Enter the First Intger");
+                        Side1 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Please Enter the second Intger");
+                        Side2 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Please Enter the third Intger");
+                        Side3 = Convert.ToInt32(Console.ReadLine());
+
+                        TriangleSolver triangleSolver = new TriangleSolver();
+
+                        Console.WriteLine("Results: " + triangleSolver.analyze(Side1, Side2, Side3));
+
+
+
+                    }
+                    else if (userSelection == "2")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Wrong Input.. Please Try Again");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Please Select 1 or 2" + ex);
+                }
+            } while (meneBool == false);
+          
+           
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TriangleSolver.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA_A02
+{
+   public class TriangleSolver
+    {
+        public string analyze (int side1, int side2, int side3)
+        {
+            
+            string typeOfTriangle= " ";
+
+
+            //if (side1 + side2 <= side3 || side1 + side3 <= side2 ||  side2 + side3 <= side1)
+            //{
+
+            //    Console.WriteLine("Numbers are NOT Valid and Do NOT Form a Triangle");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Numbers are Valid and Form a Triangle");
+
+
+            //}
+
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+            {
+                typeOfTriangle += "Error.. Please Enter all Positive Integers";
+                return typeOfTriangle;
+            }
+            else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2)
+            {
+                typeOfTriangle += "Numbers are NOT Valid and Do NOT Form a Triangle";
+                return typeOfTriangle;
+            }
+            else 
+            {
+               Console.WriteLine("Numbers are Valid and Do Form a Triangle");
+                if (side1 == side2 && side2 == side3)
+                {
+                    typeOfTriangle += "Its an equilateral triangle";
+                    return typeOfTriangle;
+
+                }
+                else if (side1 == side2 || side1 == side3 || side2 == side3)
+                {
+                    typeOfTriangle += "Its an isosceles triangle";
+                    return typeOfTriangle;
+
+                }
+                else
+                {
+                    typeOfTriangle += "Its an scalene triangle";
+                    return typeOfTriangle;
+                }
+                
+            }
+          
+
+        }
+    }
+}
